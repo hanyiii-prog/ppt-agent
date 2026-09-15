449db064a81dcfbecd12ad2441ba086cc62289e0
hanyiii-prog <hanyiii@163.com>
2026-09-15 17:26:19 +0800

    test: verify order-independent layout signatures

--- tests/test_layout_signature.py
+++ tests/test_layout_signature.py
@@ -2,7 +2,12 @@ from ppt_agent.layout_signature import canonical_layout_signature
 
 
 def test_layout_signature_is_order_and_value_stable():
-    shapes = [{"id": "a", "type": "TEXT", "z_index": 1, "geometry": {"left": 1, "top": 2, "width": 3, "height": 4}}]
-    assert canonical_layout_signature(shapes) == canonical_layout_signature(shapes)
-    changed = [{**shapes[0], "geometry": {**shapes[0]["geometry"], "width": 3.1}}]
+    shapes = [
+        {"id": "a", "type": "TEXT", "z_index": 1, "geometry": {"left": 1, "top": 2, "width": 3, "height": 4}},
+        {"id": "b", "type": "RECT", "z_index": 0, "geometry": {"left": 5, "top": 6, "width": 7, "height": 8}},
+    ]
+    reordered = list(reversed(shapes))
+    assert canonical_layout_signature(shapes) == canonical_layout_signature(reordered)
+
+    changed = [{**shapes[0], "geometry": {**shapes[0]["geometry"], "width": 3.1}}, shapes[1]]
     assert canonical_layout_signature(shapes) != canonical_layout_signature(changed)
